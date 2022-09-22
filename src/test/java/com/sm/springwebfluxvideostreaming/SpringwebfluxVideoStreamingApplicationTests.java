0140dfae339242ac736193fbb1a82ff74698c1f8
sadig muradov <sadmur2002@gmail.com>
2022-09-22 23:39:04 +0400

    master: initial changes

---
+++ src/test/java/com/sm/springwebfluxvideostreaming/SpringwebfluxVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.springwebfluxvideostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxVideoStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
